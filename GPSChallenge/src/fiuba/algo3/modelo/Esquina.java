a52e2eef09c625ffe553647da0c27038b463abac
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-03 21:57:22 +0000

    Cambios en Esquina: Los atributos ya no son de tipo Guardable. Corregido error atributo Auto tenia que ser de tipo Vehiculo

--- GPSChallenge/src/fiuba/algo3/modelo/Esquina.java
+++ GPSChallenge/src/fiuba/algo3/modelo/Esquina.java
@@ -2,25 +2,25 @@ package fiuba.algo3.modelo;
 
 public class Esquina {
 	private Posicion unaPosicion;
-	private Guardable<Auto> unAuto;
-	private Guardable<Obstaculo> unObstaculo;
-	private Guardable<Sorpresa> unaSorpresa;
+	private Vehiculo unVehiculo;
+	private Obstaculo unObstaculo;
+	private Sorpresa unaSorpresa;
 	
 	public Esquina(Posicion posicion){
 		this.unaPosicion = posicion;
 	}
 
-	public void colocarAuto(Guardable<Auto> auto){
-		this.unAuto = auto;
-		auto.actualizarEsquina(this);
+	public void colocarAuto(Vehiculo vehiculo){
+		this.unVehiculo = vehiculo;
+		vehiculo.actualizarEsquina(this);
 	}
 	
 	public void borrarAuto(){
-		this.unAuto = null;
+		this.unVehiculo = null;
 	}
 
-	public Guardable<Auto> obtenerAuto() {
-		return this.unAuto;
+	public Vehiculo obtenerAuto() {
+		return this.unVehiculo;
 	}
 	
 }
