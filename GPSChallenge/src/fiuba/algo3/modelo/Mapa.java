12c94f2a88e72cb7429e4af3eed84b94b3c5c59a
nicoacarolo@gmail.com <nicoacarolo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-04 02:35:08 +0000

    Refactor nombres de metodos en clase Mapa.

--- GPSChallenge/src/fiuba/algo3/modelo/Mapa.java
+++ GPSChallenge/src/fiuba/algo3/modelo/Mapa.java
@@ -11,35 +11,35 @@ public class Mapa {
 		columnas = columna;
 		
 		tablero = new Esquina [fila][columna];
-		for (int i = 1 ; i <= dameFilas() ; i++){
-			for (int j = 1 ; j <= dameColumnas() ; j++){
+		for (int i = 1 ; i <= devolverFilas() ; i++){
+			for (int j = 1 ; j <= devolverColumnas() ; j++){
 				Posicion posicion = new Posicion(i, j);
 				Esquina esquina = new Esquina(posicion);
-				setEsquina(esquina, i ,j );
+				colocarEsquina(esquina, i ,j );
 			}
 		}
 	}
 	
-	public int dameFilas(){
+	public int devolverFilas(){
 		return this.filas;
 	}
 	
-	public int dameColumnas(){
+	public int devolverColumnas(){
 		return this.columnas;
 	}
 	
-	public Esquina dameEsquina (int fila , int columna){
+	public Esquina devolverEsquina (int fila , int columna){
 		return tablero[fila-1][columna-1];
 	}
 	
-	public void setEsquina(Esquina unaEsquina, int fila, int columna){
+	public void colocarEsquina(Esquina unaEsquina, int fila, int columna){
 		tablero [fila-1][columna-1] = unaEsquina;
 	}
 	
 	public void moverArriba (Vehiculo unVehiculo){
 		Esquina esquinaActual = unVehiculo.devolverEsquina();
 		Posicion posicionActual = esquinaActual.devolverPosicion();
-		Esquina esquinaFutura = this.dameEsquina(posicionActual.devolverPosicionColumna() , posicionActual.devolverPosicionFila() + unVehiculo.movimientos());
+		Esquina esquinaFutura = devolverEsquina(posicionActual.devolverPosicionColumna() , posicionActual.devolverPosicionFila() + unVehiculo.movimientos());
 		esquinaFutura.colocarAuto(unVehiculo);
 		esquinaActual.borrarAuto();
 	}
