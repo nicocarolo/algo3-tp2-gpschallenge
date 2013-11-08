f842ef0d3c4a9b9012cc20fe3bc87ce790e20538
nicoacarolo@gmail.com <nicoacarolo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-08 17:37:38 +0000

    Agregada clase MapaTest

---
+++ GPSChallenge/test/fiuba/algo3/modelo/MapaTest.java
@@ -0,0 +1,21 @@
+package fiuba.algo3.modelo;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MapaTest {
+
+		@Test
+		public void testDeberiaCrearMapa() {
+			Mapa mapa = new Mapa(2 , 1);
+			Assert.assertEquals(2, mapa.dameFilas());
+			Assert.assertEquals(1, mapa.dameColumnas());
+			for (int i = 1 ; i <= mapa.dameFilas() ; i++){
+				for (int j = 1 ; j <= mapa.dameColumnas() ; j++){
+					Assert.assertNotNull(mapa.dameEsquina(i, j));
+				}
+			}
+		}
+}
