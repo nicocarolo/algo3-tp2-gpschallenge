208273f74711d639d7beb9fad174e1c479adde90
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-10 00:18:40 +0000

    -Agregada funcionalidad a la clase Desfavorable. -Agregado test a la clase DesfavorableTest.

--- GPSChallenge/test/fiuba/algo3/modelo/DesfavorableTest.java
+++ GPSChallenge/test/fiuba/algo3/modelo/DesfavorableTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.modelo.direccion.Arriba;
 import fiuba.algo3.modelo.sorpresa.Desfavorable;
+import fiuba.algo3.modelo.vehiculo.Auto;
 
 public class DesfavorableTest {
 
@@ -12,5 +14,20 @@ public class DesfavorableTest {
 	public void testDesfavorableDeberiaExistir() {
 		Desfavorable unaDesfavorable = new Desfavorable();
 		assertTrue(unaDesfavorable instanceof Desfavorable);
+	}
+	
+	@Test
+	public void testDeberiaIncrementarMovimiento() {
+		Mapa unMapa = new Mapa(8, 8);
+		Desfavorable unaSorpresaDesfavorable = new Desfavorable();
+		Esquina esquinaConSorpresa = unMapa.dameEsquina(new Posicion(5, 4));
+		esquinaConSorpresa.colocarSorpresa(unaSorpresaDesfavorable);
+		Jugador unJugador = new Jugador(new Auto(
+				unMapa.dameEsquina(new Posicion(8, 4))));
+		unJugador.setDireccion(new Arriba());
+		unJugador.jugar(unMapa);
+		unJugador.jugar(unMapa);
+		unJugador.jugar(unMapa);
+		assertTrue(unJugador.devolverMovimientosHechos() == (3 + (3 * 25 / 100)));
 	}
 }
