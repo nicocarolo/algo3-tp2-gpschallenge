3f1543f8e4a2370a49428d9e77b7a108fb657956
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-13 00:27:12 +0000

    -Corregidos tests de la clase ControlPolicialTest. -Agregado atributo probabilidad a la clase ControPolicial.

--- trunk/GPSChallenge/src/fiuba/algo3/modelo/obstaculo/ControlPolicial.java
+++ trunk/GPSChallenge/src/fiuba/algo3/modelo/obstaculo/ControlPolicial.java
@@ -6,26 +6,32 @@ import fiuba.algo3.modelo.vehiculo.Camioneta;
 import fiuba.algo3.modelo.vehiculo.Moto;
 
 public class ControlPolicial extends Obstaculo {
+	
+	private double probabilidad;
+	
+	public double obtenerProbabilidad(){
+		return this.probabilidad;
+	}
 
 	public void aplicar(Jugador unJugador) {
 		unJugador.devolverVehiculo().interactuarCon(this);
 	}
 
 	public void aplicar(Auto unAuto) {
-		double probabilidad = Math.random();
-		if (probabilidad <= 0.5)
+		this.probabilidad = Math.random();
+		if (this.probabilidad <= 0.5)
 			unAuto.devolverJugador().aumentarMovimientoHechos(3);
 	}
 
 	public void aplicar(Moto unaMoto) {
-		double probabilidad = Math.random();
-		if (probabilidad <= 0.8)
+		this.probabilidad = Math.random();
+		if (this.probabilidad <= 0.8)
 			unaMoto.devolverJugador().aumentarMovimientoHechos(3);
 	}
 
 	public void aplicar(Camioneta unaCamioneta) {
-		double probabilidad = Math.random();
-		if (probabilidad <= 0.3)
+		this.probabilidad = Math.random();
+		if (this.probabilidad <= 0.3)
 			unaCamioneta.devolverJugador().aumentarMovimientoHechos(3);
 	}
 }
