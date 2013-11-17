cbbb2525e585107de77ef35659f46adc3b4f60c6
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-17 22:09:20 +0000

    - Corregidos tests de PiqueteTest. - Tests de PiqueteTest movidos a JugadorTest. - Agregados test integradores a JugadorTest.

--- GPSChallenge/test/fiuba/algo3/modelo/obstaculo/PiqueteTest.java
+++ GPSChallenge/test/fiuba/algo3/modelo/obstaculo/PiqueteTest.java
@@ -6,14 +6,11 @@ import org.junit.Test;
 
 import fiuba.algo3.modelo.Esquina;
 import fiuba.algo3.modelo.Jugador;
-import fiuba.algo3.modelo.Mapa;
 import fiuba.algo3.modelo.Posicion;
-import fiuba.algo3.modelo.Radar;
 import fiuba.algo3.modelo.direccion.Abajo;
 import fiuba.algo3.modelo.direccion.Arriba;
 import fiuba.algo3.modelo.direccion.Derecha;
 import fiuba.algo3.modelo.direccion.Izquierda;
-import fiuba.algo3.modelo.excepcion.ExcepcionEsquinaInvalida;
 import fiuba.algo3.modelo.vehiculo.Auto;
 import fiuba.algo3.modelo.vehiculo.Camioneta;
 import fiuba.algo3.modelo.vehiculo.Moto;
@@ -21,119 +18,58 @@ import fiuba.algo3.modelo.vehiculo.Moto;
 public class PiqueteTest {
 
 	@Test
-	public void testDeberiaCambiarleLaDireccionAlAuto() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Auto unAuto = new Auto(unMapa);
+	public void testDeberiaCambiarleLaDireccionAlAuto() {
+		Esquina unaEsquinaConAutoYPiquete = new Esquina(new Posicion(3,3));
+		
+		Auto unAuto = new Auto(unaEsquinaConAutoYPiquete);
 		Jugador unJugador = new Jugador(unAuto);
-		Piquete unPiquete = new Piquete(unAuto.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(3,
-				4));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
 		unJugador.cambiarDireccion(new Derecha());
-
-		unJugador.jugar(unMapa);
+		
+		Piquete unPiquete = new Piquete(unaEsquinaConAutoYPiquete);
+		unPiquete.aplicar(unAuto);
 
 		assertTrue(unJugador.devolverDireccion() instanceof Izquierda);
 	}
 
 	@Test
-	public void testDeberiaCambiarleLaDireccionALaCamioneta() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Camioneta unaCamioneta = new Camioneta(unMapa);
+	public void testDeberiaCambiarleLaDireccionALaCamioneta() {
+		Esquina unaEsquinaConCamionetaYPiquete = new Esquina(new Posicion(3,3));
+		
+		Camioneta unaCamioneta = new Camioneta(unaEsquinaConCamionetaYPiquete);
 		Jugador unJugador = new Jugador(unaCamioneta);
-		Piquete unPiquete = new Piquete(unaCamioneta.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(4,
-				3));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
-		unJugador.cambiarDireccion(new Abajo());
-
-		unJugador.jugar(unMapa);
-
-		assertTrue(unJugador.devolverDireccion() instanceof Arriba);
-	}
-
-	@Test
-	public void testNoDeberiaCambiarleLaDireccionALaMoto() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Moto unaMoto = new Moto(unMapa);
-		Jugador unJugador = new Jugador(unaMoto);
-		Piquete unPiquete = new Piquete(unaMoto.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(4,
-				3));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
-		unJugador.cambiarDireccion(new Abajo());
-
-		unJugador.jugar(unMapa);
+		unJugador.cambiarDireccion(new Arriba());
+		
+		Piquete unPiquete = new Piquete(unaEsquinaConCamionetaYPiquete);
+		unPiquete.aplicar(unaCamioneta);
 
 		assertTrue(unJugador.devolverDireccion() instanceof Abajo);
 	}
 
 	@Test
-	public void testDeberiaAumentarleLosMovimientosALaMoto() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Moto unaMoto = new Moto(unMapa);
+	public void testNoDeberiaCambiarleLaDireccionALaMoto() {
+		Esquina unaEsquinaConMotoYPiquete = new Esquina(new Posicion(3,3));
+		
+		Moto unaMoto = new Moto(unaEsquinaConMotoYPiquete);
 		Jugador unJugador = new Jugador(unaMoto);
-		Piquete unPiquete = new Piquete(unaMoto.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(4,
-				3));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
 		unJugador.cambiarDireccion(new Abajo());
-
-		unJugador.jugar(unMapa);
-
-		assertTrue(unJugador.devolverMovimientosHechos() == 3);
+		
+		Piquete unPiquete = new Piquete(unaEsquinaConMotoYPiquete);
+		unPiquete.aplicar(unaMoto);
+		
+		assertTrue(unJugador.devolverDireccion() instanceof Abajo);
 	}
 
 	@Test
-	public void testElAutoDeberiaVolverALaPosicionOriginal() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Auto unAuto = new Auto(unMapa);
-		Jugador unJugador = new Jugador(unAuto);
-		Piquete unPiquete = new Piquete(unAuto.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(4,
-				3));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
+	public void testDeberiaAumentarleLosMovimientosALaMoto() {
+		Esquina unaEsquinaConMotoYPiquete = new Esquina(new Posicion(3,3));
+		
+		Moto unaMoto = new Moto(unaEsquinaConMotoYPiquete);
+		Jugador unJugador = new Jugador(unaMoto);
 		unJugador.cambiarDireccion(new Abajo());
+		
+		Piquete unPiquete = new Piquete(unaEsquinaConMotoYPiquete);	
+		unPiquete.aplicar(unaMoto);
 
-		unJugador.jugar(unMapa);
-
-		assert (unJugador.devolverVehiculo().devolverEsquina()
-				.devolverPosicion().equals(esquinaConObstaculo
-				.devolverPosicion()));
-	}
-
-	@Test
-	public void testElAutoDeberiaTenerLaVisibilidadCorrectaAlVolverALaPosicionOriginal() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(20, 20);
-		Radar unRadar = new Radar(unMapa);
-		Auto unAuto = new Auto(unMapa);
-		Jugador unJugador = new Jugador(unAuto);
-		Piquete unPiquete = new Piquete(unAuto.devolverEsquina());
-		Esquina esquinaConObstaculo = unMapa.devolverUnaEsquina(new Posicion(4,
-				3));
-		esquinaConObstaculo.colocarObstaculo(unPiquete);
-
-		unJugador.cambiarDireccion(new Abajo());
-
-		unJugador.jugar(unMapa);
-
-		for (int i = -2; i <= 2; i++) {
-			for (int j = -2; j <= 2; j++) {
-				boolean visibilidad = unRadar.devolverVisibilidad(unMapa
-						.devolverUnaEsquina(new Posicion(unAuto
-								.devolverEsquina().devolverPosicion()
-								.devolverPosicionFila()
-								+ i, unAuto.devolverEsquina()
-								.devolverPosicion().devolverPosicionColumna()
-								+ j)));
-
-				assertTrue(visibilidad);
-			}
-		}
+		assertTrue(unJugador.devolverMovimientosHechos() == 2);
 	}
 }
