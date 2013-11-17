c779f2e6686cc3b2a24cffaeafa8b0285706f657
mastanca14@gmail.com <mastanca14@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-17 18:09:39 +0000

    Agregados tests a Jugador Corregido test Juego Eliminado constructor vacio en Jugador

--- trunk/GPSChallenge/test/fiuba/algo3/modelo/JuegoTest.java
+++ trunk/GPSChallenge/test/fiuba/algo3/modelo/JuegoTest.java
@@ -4,12 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.modelo.excepcion.ExcepcionEsquinaInvalida;
+import fiuba.algo3.modelo.vehiculo.Moto;
+
 public class JuegoTest {
 
 	@Test
-	public void testJuegoDeberiaCrearseConMapaJugadorYRadar() {
+	public void testJuegoDeberiaCrearseConMapaJugadorYRadar() throws ExcepcionEsquinaInvalida {
 		Mapa unMapa = new Mapa(7, 7);
-		Jugador unJugador = new Jugador();
+		Moto unaMoto = new Moto(unMapa);
+		Jugador unJugador = new Jugador(unaMoto);
 		Radar unRadar = new Radar(unMapa);
 		Juego unJuego = new Juego(unMapa, unJugador, unRadar);
 		assertTrue(unJuego.dameMapa() == unMapa);
