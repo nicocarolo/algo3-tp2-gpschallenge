af11e7bffb2e163293221996592fca8361d7b2b3
mastanca14@gmail.com <mastanca14@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-20 21:04:12 +0000

    Cambiado constructor de juego

--- trunk/GPSChallenge/src/fiuba/algo3/modelo/Juego.java
+++ trunk/GPSChallenge/src/fiuba/algo3/modelo/Juego.java
@@ -1,15 +1,18 @@
 package fiuba.algo3.modelo;
 
-public class Juego {
+import fiuba.algo3.modelo.excepcion.ExcepcionEsquinaInvalida;
+import fiuba.algo3.modelo.vehiculo.Auto;
+
+public class Juego extends Observado {
 
 	private Mapa unMapa;
 	private Radar unRadar;
 	private Jugador unJugador;
 
-	public Juego(Mapa mapa, Jugador jugador, Radar radar) {
-		this.unJugador = jugador;
-		this.unMapa = mapa;
-		this.unRadar = radar;
+	public Juego() throws ExcepcionEsquinaInvalida {
+		this.unMapa = new Mapa(20, 20);
+		this.unJugador = new Jugador(new Auto(this.unMapa));
+		this.unRadar = new Radar(unMapa);
 	}
 
 	public Mapa devolverMapa() {
--- trunk/GPSChallenge/test/fiuba/algo3/modelo/JuegoTest.java
+++ trunk/GPSChallenge/test/fiuba/algo3/modelo/JuegoTest.java
@@ -5,19 +5,14 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import fiuba.algo3.modelo.excepcion.ExcepcionEsquinaInvalida;
-import fiuba.algo3.modelo.vehiculo.Moto;
 
 public class JuegoTest {
 
 	@Test
 	public void testJuegoDeberiaCrearseConMapaJugadorYRadar() throws ExcepcionEsquinaInvalida {
-		Mapa unMapa = new Mapa(7, 7);
-		Moto unaMoto = new Moto(unMapa);
-		Jugador unJugador = new Jugador(unaMoto);
-		Radar unRadar = new Radar(unMapa);
-		Juego unJuego = new Juego(unMapa, unJugador, unRadar);
-		assertTrue(unJuego.devolverMapa() == unMapa);
-		assertTrue(unJuego.devolverJugador() == unJugador);
-		assertTrue(unJuego.devolverRadar() == unRadar);
+		Juego unJuego = new Juego();
+		assertTrue(unJuego.devolverMapa() instanceof Mapa);
+		assertTrue(unJuego.devolverJugador() instanceof Jugador);
+		assertTrue(unJuego.devolverRadar() instanceof Radar);
 	}
 }
