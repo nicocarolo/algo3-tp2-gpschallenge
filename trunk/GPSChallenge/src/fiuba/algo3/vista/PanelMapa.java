338d85855ffb9d0004836a2882b445253f4d6684
nicoacarolo@gmail.com <nicoacarolo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-20 17:14:05 +0000

    Mejorada la gui.

--- trunk/GPSChallenge/src/fiuba/algo3/vista/PanelMapa.java
+++ trunk/GPSChallenge/src/fiuba/algo3/vista/PanelMapa.java
@@ -1,126 +1,119 @@
 package fiuba.algo3.vista;
+
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 public class PanelMapa extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private JButton auto;
+
+	private JLabel auto;
 	//private ImageIcon bgImage;
-	private int posX = 3;
-	private int posY = 3;
-	private ImageIcon bgImage;
+	private int posX = 9;
+	private int posY = 0;
 	
 	public PanelMapa(){
 		
 		setLayout(null);
 		
-//		JButton btnNewButton = new JButton("");
-//		//btnNewButton.setBackground(new Color(50, 205, 50));
-//		btnNewButton.setOpaque(false);
-//		btnNewButton.setContentAreaFilled(false);
-//		btnNewButton.setBounds(50, 35, 89, 80);
-//		add(btnNewButton);
-//		
-//		JButton button = new JButton("");
-//		button.setBackground(new Color(50, 205, 50));
-//		button.setBounds(194, 35, 89, 80);
-//		add(button);
-//		
-//		JButton button_1 = new JButton("");
-//		button_1.setBackground(new Color(50, 205, 50));
-//		button_1.setBounds(482, 35, 89, 80);
-//		add(button_1);
-//		
-//		JButton button_2 = new JButton("");
-//		button_2.setBackground(new Color(50, 205, 50));
-//		button_2.setBounds(338, 35, 89, 80);
-//		add(button_2);
-//		
-//		JButton button_3 = new JButton("");
-//		button_3.setBackground(new Color(50, 205, 50));
-//		button_3.setBounds(482, 155, 89, 80);
-//		add(button_3);
-//		
-//		JButton button_4 = new JButton("");
-//		button_4.setBackground(new Color(50, 205, 50));
-//		button_4.setBounds(338, 155, 89, 80);
-//		add(button_4);
-//		
-//		JButton button_5 = new JButton("");
-//		button_5.setBackground(new Color(50, 205, 50));
-//		button_5.setBounds(194, 155, 89, 80);
-//		add(button_5);
-//		
-//		JButton button_6 = new JButton("");
-//		button_6.setBackground(new Color(50, 205, 50));
-//		button_6.setBounds(50, 155, 89, 80);
-//		add(button_6);
-//		
-//		JButton button_7 = new JButton("");
-//		button_7.setBackground(new Color(50, 205, 50));
-//		button_7.setBounds(482, 275, 89, 80);
-//		add(button_7);
-//		
-//		JButton button_8 = new JButton("");
-//		button_8.setBackground(new Color(50, 205, 50));
-//		button_8.setBounds(338, 275, 89, 80);
-//		add(button_8);
-//		
-//		JButton button_9 = new JButton("");
-//		button_9.setBackground(new Color(50, 205, 50));
-//		button_9.setBounds(194, 275, 89, 80);
-//		add(button_9);
-//		
-//		JButton button_10 = new JButton("");
-//		button_10.setBackground(new Color(50, 205, 50));
-//		button_10.setBounds(50, 275, 89, 80);
-//		add(button_10);
-//		
-//		JButton button_11 = new JButton("");
-//		button_11.setBackground(new Color(50, 205, 50));
-//		button_11.setBounds(50, 395, 89, 80);
-//		add(button_11);
-//		
-//		JButton button_12 = new JButton("");
-//		button_12.setBackground(new Color(50, 205, 50));
-//		button_12.setBounds(194, 395, 89, 80);
-//		add(button_12);
-//		
-//		JButton button_13 = new JButton("");
-//		button_13.setBackground(new Color(50, 205, 50));
-//		button_13.setBounds(338, 395, 89, 80);
-//		add(button_13);
-//		
-//		JButton button_14 = new JButton("");
-//		button_14.setBackground(new Color(50, 205, 50));
-//		button_14.setBounds(482, 395, 89, 80);
-//		add(button_14);
-//		
-		auto = new JButton("");
-		auto.setIcon(new ImageIcon(getClass().getResource("/fiuba/algo3/vista/imagenes/car.jpg")));
+		auto = new JLabel("");
+		auto.setIcon(new ImageIcon(PanelMapa.class.getResource("/fiuba/algo3/vista/imagenes/car.png")));
 		auto.setBackground(new Color(0, 0, 139));
-		auto.setBounds(posX, posY, 44, 23);
+		auto.setBounds(8, 0, 32, 38);
 		add(auto);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setBackground(new Color(50, 205, 50));
+		btnNewButton.setBounds(50, 35, 89, 80);
+		add(btnNewButton);
+		
+		JButton button = new JButton("");
+		button.setBackground(new Color(50, 205, 50));
+		button.setBounds(194, 35, 89, 80);
+		add(button);
+		
+		JButton button_1 = new JButton("");
+		button_1.setBackground(new Color(50, 205, 50));
+		button_1.setBounds(482, 35, 89, 80);
+		add(button_1);
+		
+		JButton button_2 = new JButton("");
+		button_2.setBackground(new Color(50, 205, 50));
+		button_2.setBounds(338, 35, 89, 80);
+		add(button_2);
+		
+		JButton button_3 = new JButton("");
+		button_3.setBackground(new Color(50, 205, 50));
+		button_3.setBounds(482, 155, 89, 80);
+		add(button_3);
+		
+		JButton button_4 = new JButton("");
+		button_4.setBackground(new Color(50, 205, 50));
+		button_4.setBounds(338, 155, 89, 80);
+		add(button_4);
+		
+		JButton button_5 = new JButton("");
+		button_5.setBackground(new Color(50, 205, 50));
+		button_5.setBounds(194, 155, 89, 80);
+		add(button_5);
+		
+		JButton button_6 = new JButton("");
+		button_6.setBackground(new Color(50, 205, 50));
+		button_6.setBounds(50, 155, 89, 80);
+		add(button_6);
+		
+		JButton button_7 = new JButton("");
+		button_7.setBackground(new Color(50, 205, 50));
+		button_7.setBounds(482, 275, 89, 80);
+		add(button_7);
+		
+		JButton button_8 = new JButton("");
+		button_8.setBackground(new Color(50, 205, 50));
+		button_8.setBounds(338, 275, 89, 80);
+		add(button_8);
+		
+		JButton button_9 = new JButton("");
+		button_9.setBackground(new Color(50, 205, 50));
+		button_9.setBounds(194, 275, 89, 80);
+		add(button_9);
+		
+		JButton button_10 = new JButton("");
+		button_10.setBackground(new Color(50, 205, 50));
+		button_10.setBounds(50, 275, 89, 80);
+		add(button_10);
+		
+		JButton button_11 = new JButton("");
+		button_11.setBackground(new Color(50, 205, 50));
+		button_11.setBounds(50, 395, 89, 80);
+		add(button_11);
+		
+		JButton button_12 = new JButton("");
+		button_12.setBackground(new Color(50, 205, 50));
+		button_12.setBounds(194, 395, 89, 80);
+		add(button_12);
+		
+		JButton button_13 = new JButton("");
+		button_13.setBackground(new Color(50, 205, 50));
+		button_13.setBounds(338, 395, 89, 80);
+		add(button_13);
+		
+		JButton button_14 = new JButton("");
+		button_14.setBackground(new Color(50, 205, 50));
+		button_14.setBounds(482, 395, 89, 80);
+		add(button_14);
 		
 	}
 	
 	public void paintComponent (Graphics g){
-		Dimension tamanio = getSize();
-		this.bgImage = new ImageIcon(getClass().getResource("/fiuba/algo3/vista/imagenes/mapa.jpg"));
-		g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
-		setOpaque(false);
+		//Dimension tamanio = getSize();
+		//this.bgImage = new ImageIcon(getClass().getResource("/Images/mapa.jpg"));
+		//g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+		//setOpaque(false);
 		super.paintComponent(g);
-		auto.setBounds(posX, posY, 44, 23);
+		auto.setBounds(posX, posY, 32, 38);
 		//g.fillRect(posX, posY, 20, 20);
 		}
 	
---
+++ trunk/GPSChallenge/src/fiuba/algo3/vista/VentanaJuego.java
@@ -0,0 +1,154 @@
+package fiuba.algo3.vista;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaJuego extends JFrame {
+
+	private JPanel contentPane;
+	private PanelMapa panelMapa;
+	private final Action action = new SwingAction();
+
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaJuego() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnArriba = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonArriba.png")));
+		btnArriba.setBorder(BorderFactory.createEmptyBorder());
+		btnArriba.setContentAreaFilled(false);
+		btnArriba.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelMapa.moverIzquierda();
+			}
+		});
+		btnArriba.setBounds(98, 512, 126, 52);
+		contentPane.add(btnArriba);
+
+		panelMapa = new PanelMapa();
+		panelMapa.setBounds(5, 5, 674, 507);
+		contentPane.add(panelMapa);
+		panelMapa.setLayout(null);
+
+
+		JButton btnIzquierda = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonIzquierda.png")));
+		btnIzquierda.setBorder(BorderFactory.createEmptyBorder());
+		btnIzquierda.setContentAreaFilled(false);
+		btnIzquierda.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelMapa.moverIzquierda();
+			}
+		});
+		btnIzquierda.setBounds(30, 557, 126, 52);
+		contentPane.add(btnIzquierda);
+
+		JButton btnDerecha = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonDerecha.png")));
+		btnDerecha.setBorder(BorderFactory.createEmptyBorder());
+		btnDerecha.setContentAreaFilled(false);
+		btnDerecha.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelMapa.moverDerecha();
+			}
+		});
+		btnDerecha.setBounds(161, 555, 134, 57);
+		contentPane.add(btnDerecha);
+
+		JButton btnAbajo = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonAbajo.png")));
+		btnAbajo.setBorder(BorderFactory.createEmptyBorder());
+		btnAbajo.setContentAreaFilled(false);
+		btnAbajo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelMapa.moverAbajo();
+			}
+		});
+		btnAbajo.setBounds(92, 602, 141, 52);
+		contentPane.add(btnAbajo);
+
+		JButton btnNewButton = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonSalirMapa.png")));
+		btnNewButton.setBorder(BorderFactory.createEmptyBorder());
+		btnNewButton.setContentAreaFilled(false);
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int eleccion = JOptionPane.showConfirmDialog(null,
+						"Desea salir sin guardar?");
+				if (eleccion == 0) {
+					JOptionPane.showMessageDialog(null,
+							"Saliendo del GPSChallenge...");
+					System.exit(0);
+				}
+			}
+		});
+		btnNewButton.setBounds(456, 609, 140, 46);
+		contentPane.add(btnNewButton);
+
+		JButton btnNewButton_1 = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonMenuMapa.png")));
+		btnNewButton_1.setBorder(BorderFactory.createEmptyBorder());
+		btnNewButton_1.setContentAreaFilled(false);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				VentanaPpal unaVentanaPpal = new VentanaPpal();
+				unaVentanaPpal.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(456, 566, 141, 46);
+		contentPane.add(btnNewButton_1);
+
+		JButton btnNewButton_2 = new JButton(new ImageIcon(
+				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonGuardarMapa.png")));
+		btnNewButton_2.setBorder(BorderFactory.createEmptyBorder());
+		btnNewButton_2.setContentAreaFilled(false);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Guardando partida...");
+			}
+		});
+		btnNewButton_2.setBounds(456, 523, 141, 46);
+		contentPane.add(btnNewButton_2);
+
+		// panel_2.setBackgroundImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Samsung\\EclipseWorkspace\\GPSChallenge\\images\\bgImage.jpg"));
+	}
+
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+
+}
