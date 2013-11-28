6b328b73999d70660284c3f633a33ca2e2460ee0
mastanca14@gmail.com <mastanca14@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-11-28 04:03:00 +0000

    Corregida posicion de botones en VentanaJuego, ahora pertenecen al panelInformacion

--- GPSChallenge/src/fiuba/algo3/vista/VentanaJuego.java
+++ GPSChallenge/src/fiuba/algo3/vista/VentanaJuego.java
@@ -34,71 +34,54 @@ public class VentanaJuego extends JFrame implements KeyListener {
 	private JPanel panelObstaculos;
 	private final Action action = new SwingAction();
 	private boolean seGuardoJuego = false;
-    private ControladorJuego unControladorJuego;
-    private Juego unJuego;
-    private JTextField textField;
-   // private VistaVisibilidad unaVistaVisibilidad;
+	private ControladorJuego unControladorJuego;
+	private Juego unJuego;
+	private JTextField textField;
+
+	// private VistaVisibilidad unaVistaVisibilidad;
 
 	/**
 	 * Create the frame.
-	 * @throws ExcepcionEsquinaInvalida 
+	 * 
+	 * @throws ExcepcionEsquinaInvalida
 	 */
 	public VentanaJuego() throws ExcepcionEsquinaInvalida {
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 881, 768);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		addKeyListener(this);
 		setFocusable(true);
 		this.unJuego = new Juego();
-		
-		//this.unaVistaVisibilidad = new VistaVisibilidad((unJuego.devolverJugador().devolverVehiculo().devolverEsquina().devolverPosicion().devolverPosicionFila()-1)*(40+35),((unJuego.devolverJugador().devolverVehiculo().devolverEsquina().devolverPosicion().devolverPosicionColumna()-1)*(40+42)));
-		
+
+		// this.unaVistaVisibilidad = new
+		// VistaVisibilidad((unJuego.devolverJugador().devolverVehiculo().devolverEsquina().devolverPosicion().devolverPosicionFila()-1)*(40+35),((unJuego.devolverJugador().devolverVehiculo().devolverEsquina().devolverPosicion().devolverPosicionColumna()-1)*(40+42)));
+
 		panelMapa = new PanelMapa(this.unJuego.devolverMapa(), this.unJuego);
-		panelMapa.setBounds(5, 5, 660, 730);    
+		panelMapa.setBounds(5, 5, 660, 730);
 		contentPane.add(panelMapa);
 		panelMapa.setLayout(null);
-		
+
 		panelObstaculos = new JPanel();
 		panelObstaculos.setOpaque(false);
 		panelObstaculos.setBounds(5, 5, 660, 730);
-		
+
 		PanelInformacion panelInformacion = new PanelInformacion();
 		panelInformacion.setBounds(675, 5, 180, 713);
 		contentPane.add(panelInformacion);
 		panelInformacion.setLayout(null);
-		
-		this.unControladorJuego = new ControladorJuego(this.unJuego, panelMapa, panelInformacion);
 
-		/*JButton btnSalirMapa = new JButton(new ImageIcon(
-				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonSalirMapa.png")));
-		btnSalirMapa.setBorder(BorderFactory.createEmptyBorder());
-		btnSalirMapa.setContentAreaFilled(false);
-		btnSalirMapa.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int eleccion = 0;
-				if (!seGuardoJuego){
-					eleccion = JOptionPane.showConfirmDialog(null,
-						"Desea salir sin guardar?");
-				}
-				if (eleccion == 0) {
-					JOptionPane.showMessageDialog(null,
-							"Saliendo del GPSChallenge...");
-					System.exit(0);
-				}
-				
-			}
-		});
-		btnSalirMapa.setBounds(456, 609, 140, 46);
-		contentPane.add(btnSalirMapa);
+		this.unControladorJuego = new ControladorJuego(this.unJuego, panelMapa,
+				panelInformacion);
 
-		JButton btnMenuMapa = new JButton(new ImageIcon(
-				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonMenuMapa.png")));
+		JButton btnMenuMapa = new JButton(
+				new ImageIcon(
+						VentanaJuego.class
+								.getResource("/fiuba/algo3/vista/imagenes/botonMenuMapa.png")));
 		btnMenuMapa.setBorder(BorderFactory.createEmptyBorder());
 		btnMenuMapa.setContentAreaFilled(false);
 		btnMenuMapa.addActionListener(new ActionListener() {
@@ -108,25 +91,50 @@ public class VentanaJuego extends JFrame implements KeyListener {
 				unaVentanaPpal.setVisible(true);
 			}
 		});
-		btnMenuMapa.setBounds(456, 566, 141, 46);
-		contentPane.add(btnMenuMapa);
 
-		JButton btnGuardarMapa = new JButton(new ImageIcon(
-				VentanaJuego.class.getResource("/fiuba/algo3/vista/imagenes/botonGuardarMapa.png")));
+		JButton btnSalirMapa = new JButton(
+				new ImageIcon(
+						VentanaJuego.class
+								.getResource("/fiuba/algo3/vista/imagenes/botonSalirMapa.png")));
+		btnSalirMapa.setBorder(BorderFactory.createEmptyBorder());
+		btnSalirMapa.setContentAreaFilled(false);
+		btnSalirMapa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int eleccion = 0;
+				if (!seGuardoJuego) {
+					eleccion = JOptionPane.showConfirmDialog(null,
+							"Desea salir sin guardar?");
+				}
+				if (eleccion == 0) {
+					// JOptionPane.showMessageDialog(null,
+					// "Saliendo del GPSChallenge...");
+					System.exit(0);
+				}
+
+			}
+		});
+		btnSalirMapa.setBounds(19, 644, 151, 58);
+		panelInformacion.add(btnSalirMapa);
+		btnMenuMapa.setBounds(19, 510, 151, 62);
+		panelInformacion.add(btnMenuMapa);
+
+		JButton btnGuardarMapa = new JButton(
+				new ImageIcon(
+						VentanaJuego.class
+								.getResource("/fiuba/algo3/vista/imagenes/botonGuardarMapa.png")));
 		btnGuardarMapa.setBorder(BorderFactory.createEmptyBorder());
 		btnGuardarMapa.setContentAreaFilled(false);
 		btnGuardarMapa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//EN ESTA PARTE SE LLAMARIA AL METODO QUE SE ENCARGA DE LA PERSISITENCIA
+				// EN ESTA PARTE SE LLAMARIA AL METODO QUE SE ENCARGA DE LA
+				// PERSISITENCIA
 				seGuardoJuego = true;
 				JOptionPane.showMessageDialog(null, "Guardando partida...");
 			}
 		});
-		btnGuardarMapa.setBounds(456, 523, 141, 46);
-		contentPane.add(btnGuardarMapa);*/
-		
-
-		// panel_2.setBackgroundImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Samsung\\EclipseWorkspace\\GPSChallenge\\images\\bgImage.jpg"));
+		btnGuardarMapa.setBounds(19, 583, 151, 50);
+		panelInformacion.add(btnGuardarMapa);
 	}
 
 	private class SwingAction extends AbstractAction {
@@ -142,35 +150,35 @@ public class VentanaJuego extends JFrame implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()){
-			case KeyEvent.VK_UP:
-				this.unJuego.jugar(new Arriba());
-				//panelMapa.moverArriba();
-				break;
-			case KeyEvent.VK_DOWN:
-				this.unJuego.jugar(new Abajo());
-				//panelMapa.moverAbajo();
-				break;
-			case KeyEvent.VK_RIGHT:
-				this.unJuego.jugar(new Derecha());
-				//panelMapa.moverDerecha();
-				break;
-			case KeyEvent.VK_LEFT:
-				this.unJuego.jugar(new Izquierda());
-				//panelMapa.moverIzquierda();
-				break;
-		}	
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			this.unJuego.jugar(new Arriba());
+			// panelMapa.moverArriba();
+			break;
+		case KeyEvent.VK_DOWN:
+			this.unJuego.jugar(new Abajo());
+			// panelMapa.moverAbajo();
+			break;
+		case KeyEvent.VK_RIGHT:
+			this.unJuego.jugar(new Derecha());
+			// panelMapa.moverDerecha();
+			break;
+		case KeyEvent.VK_LEFT:
+			this.unJuego.jugar(new Izquierda());
+			// panelMapa.moverIzquierda();
+			break;
+		}
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
