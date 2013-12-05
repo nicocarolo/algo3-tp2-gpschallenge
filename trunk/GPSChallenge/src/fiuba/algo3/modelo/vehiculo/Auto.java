df644afa0d134af0f35957b8d4bf9ebc339777dd
ezequiel.grillo@gmail.com <ezequiel.grillo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-12-05 07:07:25 +0000

    Corregidos detalles que habian quedado mal en el commit anterior.

--- trunk/GPSChallenge/src/fiuba/algo3/modelo/vehiculo/Auto.java
+++ trunk/GPSChallenge/src/fiuba/algo3/modelo/vehiculo/Auto.java
@@ -6,17 +6,23 @@ import fiuba.algo3.modelo.obstaculo.Obstaculo;
 
 public class Auto extends Vehiculo {
 
-//	public Auto(Mapa unMapa) throws ExcepcionEsquinaInvalida {
-//		super(unMapa);
-//	}
+	// public Auto(Mapa unMapa) throws ExcepcionEsquinaInvalida {
+	// super(unMapa);
+	// }
 
+	// Si un constructor no invoca explícitamente un constructor de superclase,
+	// el compilador de Java inserta automáticamente una llamada al constructor
+	// sin argumentos de la superclase. Si la superclase no tiene un constructor
+	// sin argumentos, obtendrá un error en tiempo de compilación. Object tiene
+	// tal constructor, así que si Object es la única superclase, no hay
+	// problema.
 	public Auto(Esquina unaEsquina) {
 		super(unaEsquina);
 		seCambio();
 		notificarObservadores(this);
 	}
-	
-	public void setearEsquina(Esquina nuevaEsquina){
+
+	public void setearEsquina(Esquina nuevaEsquina) {
 		super.setearEsquina(nuevaEsquina);
 		seCambio();
 		notificarObservadores(this);
