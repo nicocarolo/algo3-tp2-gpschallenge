0a1720d190be71163592e72fe63b8c2cef43e2be
mastanca14@gmail.com <mastanca14@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-12-08 21:47:11 +0000

    Agragada la clase GuardadorPuntaje y Puntaje con respectivos tests

---
+++ trunk/GPSChallenge/test/fiuba/algo3/modelo/GuardadorPuntajesTest.java
@@ -0,0 +1,28 @@
+package fiuba.algo3.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuardadorPuntajesTest {
+
+	@Test
+	public void testDeberiaGuardarPuntajes() {
+		
+		GuardadorPuntajes guardador = new GuardadorPuntajes();
+		guardador.guardarPuntaje("Pepe", 1000);
+		
+		assertTrue(guardador.get("Pepe") == 1000);
+	}
+	
+	@Test
+	public void testDeberiaObtenerElPuntajeConElNombreCorrespondiente(){
+
+		GuardadorPuntajes guardador = new GuardadorPuntajes();
+		Puntaje puntaje = new Puntaje("Alfio", 200);
+		guardador.guardarPuntaje("Alfio", 200);
+		
+		assertTrue(guardador.obtenerPuntaje(0).equals(puntaje));
+	}
+
+}
