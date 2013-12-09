90eaca8c7bd0557ea716d296f9961e523eb3306e
nicoacarolo@gmail.com <nicoacarolo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-12-09 20:51:52 +0000

    Modificado GuardadorPuntaje-[ahora la clave es la puntuacion, y el valor el nombre del jugador] y ordenamiento de forma descendente en funcion al puntaje.

--- trunk/GPSChallenge/src/fiuba/algo3/modelo/GuardadorPuntajes.java
+++ trunk/GPSChallenge/src/fiuba/algo3/modelo/GuardadorPuntajes.java
@@ -1,28 +1,42 @@
 package fiuba.algo3.modelo;
 
+import java.io.Console;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 import java.util.TreeMap;
 
-public class GuardadorPuntajes extends TreeMap<String, Integer> {
+public class GuardadorPuntajes {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private ArrayList<String> listaJugadores = new ArrayList<String>();
+
+	private TreeMap<Integer, String> diccionario = new TreeMap<Integer, String>(Collections.reverseOrder());
 
 	public void guardarPuntaje(String nombreJugador, int puntaje) {
-		put(nombreJugador, puntaje);
-		listaJugadores.add(nombreJugador);
+		diccionario.put(puntaje, nombreJugador);
+		
+		//listaJugadores.add(nombreJugador);
 	}
 
 	public Puntaje obtenerPuntaje(int posicion) {
-		int valor = get(listaJugadores.get(posicion));
-		String nombre = listaJugadores.get(posicion);
-		Puntaje puntaje = new Puntaje(nombre, valor);
+		//lista de entradas del diccionario
+		ArrayList<Map.Entry<Integer, String>> list = new ArrayList<Map.Entry<Integer, String>>(this.diccionario.entrySet());
+		//pido la entrada en la posicion pedida
+		Map.Entry<Integer, String> entrada = list.get(posicion);
+		//le pido el valor(nombre del Jugador) a la entrada
+		String nombre = entrada.getValue();
+		//le pido la clave(puntuacion del Jugador) a la entrada
+		int puntuacion = entrada.getKey();
+		Puntaje puntaje = new Puntaje(nombre, puntuacion);
 		return puntaje;
 
 	}
-
+	
+	public void ordenarPorValor(){
+		
+	}
 }
