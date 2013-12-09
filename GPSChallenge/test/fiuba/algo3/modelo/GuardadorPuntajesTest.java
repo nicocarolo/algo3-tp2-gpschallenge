54b166acc58c514d3de351fed04611af28563f84
nicoacarolo@gmail.com <nicoacarolo@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-12-09 20:51:52 +0000

    Modificado GuardadorPuntaje-[ahora la clave es la puntuacion, y el valor el nombre del jugador] y ordenamiento de forma descendente en funcion al puntaje.

--- GPSChallenge/test/fiuba/algo3/modelo/GuardadorPuntajesTest.java
+++ GPSChallenge/test/fiuba/algo3/modelo/GuardadorPuntajesTest.java
@@ -2,6 +2,8 @@ package fiuba.algo3.modelo;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class GuardadorPuntajesTest {
@@ -12,7 +14,10 @@ public class GuardadorPuntajesTest {
 		GuardadorPuntajes guardador = new GuardadorPuntajes();
 		guardador.guardarPuntaje("Pepe", 1000);
 		
-		assertTrue(guardador.get("Pepe") == 1000);
+		System.out.println(guardador.obtenerPuntaje(0).getNombre());
+		System.out.println(guardador.obtenerPuntaje(0).getValor());
+		
+		assertTrue(guardador.obtenerPuntaje(0).equals(new Puntaje("Pepe", 1000)));
 	}
 	
 	@Test
@@ -23,6 +28,22 @@ public class GuardadorPuntajesTest {
 		guardador.guardarPuntaje("Alfio", 200);
 		
 		assertTrue(guardador.obtenerPuntaje(0).equals(puntaje));
+	}
+	
+	@Test
+	public void testDeberiaOrdenarLosPuntajes(){
+		GuardadorPuntajes guardador = new GuardadorPuntajes();
+		guardador.guardarPuntaje("Alfio", 200);
+		guardador.guardarPuntaje("Pepe", 1000);
+		guardador.guardarPuntaje("Otro", 500);
+		
+		Puntaje puntaje = new Puntaje("Alfio", 200);
+		Puntaje puntaje2 = new Puntaje("Pepe", 1000);
+		Puntaje puntaje3 = new Puntaje("Otro", 500);
+		
+		assertTrue(guardador.obtenerPuntaje(0).equals(puntaje2));
+		assertTrue(guardador.obtenerPuntaje(1).equals(puntaje3));
+		assertTrue(guardador.obtenerPuntaje(2).equals(puntaje));
 	}
 
 }
