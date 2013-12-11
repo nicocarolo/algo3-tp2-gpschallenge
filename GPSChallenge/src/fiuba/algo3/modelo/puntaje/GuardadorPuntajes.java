e75183a5169a892e37d7fb07339ecf0942c342c0
mastanca14@gmail.com <mastanca14@gmail.com@f57558df-9300-98d6-ee0d-23c1758fd126>
2013-12-11 14:22:51 +0000

    Corregido guardador de puntajes pinchaba si no existia el archivo Ranking

--- GPSChallenge/src/fiuba/algo3/modelo/puntaje/GuardadorPuntajes.java
+++ GPSChallenge/src/fiuba/algo3/modelo/puntaje/GuardadorPuntajes.java
@@ -47,4 +47,14 @@ public class GuardadorPuntajes {
 		}
 		return xmlElement;
 	}
+
+	public void eliminarPuntaje(Puntaje puntajeApertura) {
+		for(int i =0; i<puntajesTotales.size();i++){
+			Puntaje puntajeObtenido = devolverPuntaje(i);
+			if(puntajeObtenido.equals(puntajeApertura)){
+				puntajesTotales.remove(i);
+			}
+		}
+		
+	}
 }
